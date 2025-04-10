714696d45248d32585d5957b2d48820ca526684e
Lorenzo Delgado <lorenzo@edgeandnode.com>
2025-04-10 18:43:59 +0200

    feat(seahaven-cli): add 'down' command (#53)
    - Introduced a new `down` command to the CLI for stopping and removing Docker containers, networks, images, and volumes.
    - Updated the command structure to include the `down` command alongside existing commands.
    - Enhanced the `run` function to handle the new `down` command and its arguments, including support for a dry run mode and service specification.
    
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

---
+++ seahaven-cli/src/cmd/down.rs
@@ -0,0 +1,108 @@
+use std::{fs::File, io::BufReader, path::PathBuf};
+
+use seahaven_docker::cmd::{DockerCmd, IntoCommand};
+
+use crate::result::{Error, Result};
+
+/// The `down` command name
+pub(super) const CMD: &str = "down";
+
+/// Create the `down` command
+pub(super) fn cmd() -> clap::Command {
+    clap::command!(CMD)
+        .about("Stop and remove containers, networks, images, and volumes")
+        .args([
+            clap::arg!(-f --file <FILE> "The seahaven setup file")
+                .default_value("setup.yaml")
+                .value_parser(clap::value_parser!(PathBuf)),
+            clap::arg!(-v --volumes "Remove named volumes declared in the volumes section of the Compose file")
+                .action(clap::ArgAction::SetTrue),
+            clap::arg!(--dry-run "Execute command in dry run mode")
+                .action(clap::ArgAction::SetTrue),
+            clap::arg!([SERVICE] ... "The services to stop and remove")
+                .action(clap::ArgAction::Append),
+        ])
+}
+
+/// The `down` command implementation
+pub(super) async fn run(matches: &clap::ArgMatches) -> Result<()> {
+    let setup_file = matches
+        .get_one::<PathBuf>("file")
+        .expect("Failed to get setup file");
+    if !setup_file.exists() {
+        return Err(anyhow::anyhow!("Setup file not found: {}", setup_file.display()).into());
+    }
+
+    // Load the setup file
+    let setup_file = File::open(setup_file)
+        .map(BufReader::new)
+        .map_err(|err| anyhow::anyhow!("Failed to open setup file: {err}"))?;
+    let (env, content) = seahaven_file::from_reader(setup_file)
+        .map_err(|err| anyhow::anyhow!("Failed to parse setup file: {err}"))?
+        .unpack();
+
+    // Transform the content into a compose file
+    let compose = seahaven_file::try_into_compose_file(content)
+        .map_err(|err| anyhow::anyhow!("Invalid setup file: {err}"))?;
+
+    // Create a tempfile for the env and the content
+    let temp_dir = tempfile::Builder::new()
+        .prefix("seahaven-")
+        .tempdir()
+        .map_err(|err| anyhow::anyhow!("Failed to create tempdir: {err}"))?;
+    let temp_dir_path = temp_dir.path();
+    tracing::debug!("Compose temporary directory: {}", temp_dir_path.display());
+
+    let env_file_path = temp_dir_path.join(".env");
+    let content_file_path = temp_dir_path.join("docker-compose.yaml");
+
+    {
+        let env_file = File::create(&env_file_path)
+            .map_err(|err| anyhow::anyhow!("Failed to create .env file: {err}"))?;
+
+        let content_file = File::create(&content_file_path)
+            .map_err(|err| anyhow::anyhow!("Failed to create docker-compose.yaml file: {err}"))?;
+
+        tracing::debug!("Writing .env file: {}", env_file_path.display());
+        seahaven_file::serde_envfile::to_writer(env_file, &env)
+            .map_err(|err| anyhow::anyhow!("Failed to write .env file: {err}"))?;
+
+        tracing::debug!(
+            "Writing docker-compose.yaml file: {}",
+            content_file_path.display()
+        );
+        seahaven_file::seahaven_compose_file::ser::to_writer(content_file, &compose)
+            .map_err(|err| anyhow::anyhow!("Failed to write docker-compose.yaml file: {err}"))?;
+    }
+
+    let mut command = DockerCmd::new()
+        .compose()
+        .with_file(content_file_path)
+        .with_env_file(env_file_path)
+        .with_plain_progress()
+        .down()
+        .with_volumes(matches.get_flag("volumes"))
+        .with_dry_run(matches.get_flag("dry-run"))
+        .with_services(matches.get_many::<String>("SERVICE").unwrap_or_default())
+        .into_command();
+
+    tracing::debug!("Running command: {:?}", command.as_std());
+
+    let mut child = command
+        .kill_on_drop(true)
+        .spawn()
+        .map_err(|err| anyhow::anyhow!("Failed to spawn docker compose down command: {err}"))?;
+
+    let status = child
+        .wait()
+        .await
+        .map_err(|err| anyhow::anyhow!("Failed to wait for docker compose down command: {err}"))?;
+    if !status.success() {
+        return Err(
+            Error::new(anyhow::anyhow!("Docker compose down command failed"))
+                .with_code(status.code().unwrap_or(1)),
+        );
+    }
+
+    Ok(())
+}
